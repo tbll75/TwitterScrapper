1c590a1095d0b404717466ebc33828e787ce2795
Thibault LOUIS-LUCAS <tbll75@gmail.com>
2019-08-17 12:57:55 +0200

    Add deploy config

---
+++ ecosystem.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    apps: [{
+      name: 'TwitterScrapper',
+      script: './index.js'
+    }],
+    deploy: {
+      production: {
+        user: 'ubuntu',
+        host: 'ec2-34-245-190-171.eu-west-1.compute.amazonaws.com',
+        //key: '~/.ssh/tutorial-2.pem',
+        key: '/Users/thib/.ssh/TwitterScrapper.pem',
+        ref: 'origin/master',
+        repo: 'git@github.com:tbll75/TwitterScrapper.git',
+        path: '/home/bitnami',
+        'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+      }
+    }
+  }
